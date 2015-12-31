6062af3c7d6b2b2b1b8511f1f7446b053bb90ff6
BRJennings <jennings.brendanr@gmail.com>
2015-12-31 12:53:12 -0800

    added mysql db

--- stocks.py
+++ stocks.py
@@ -1,27 +1,45 @@
 # Gets current stock price for all stocks on the TSX/SP composite index.
 # Pulls the data from ca.finance.yahoo.com.
+# Author: Brendan Jennings
 
 import urllib
 import re
 import MySQLdb
+from getpass import getpass
 
-# open symbol file and read it, split on delimiter (\n)
-symbolfile = open("symbols.txt")
-symbolslist = symbolfile.read()
-symbolslist = symbolslist.split("\n")
+# get username and password
+username = raw_input("User name: ")
+password = getpass()
 
+# Open database connection
+db = MySQLdb.connect("localhost", username, password, "stock_info")
+
+# Prepare symbol and price cursor objects
+symCursor = db.cursor()
+priCursor = db.cursor()
+
+# get number of stocks in database to scrape
+symCursor.execute("select symbol from stock")
+numStocks = symCursor.rowcount
 
 i = 0
-while i <= len(symbolslist):
-    symbol = symbolslist[i]
+while i <= numStocks:
+    symbol = str(symCursor.fetchone())
+    symbol = symbol[2:-3]
     htmlfile = urllib.urlopen("https://ca.finance.yahoo.com/q?s=" 
         + symbol + "&ql=0")
     htmltext = htmlfile.read()
     symbol = symbol.lower()
-    regex ='<span id="yfs_l84_' + symbol + '*">(.+?)</span>'
+    regex ='<span id="yfs_l84_' + symbol + '">(.+?)</span>'
     pattern = re.compile(regex)
     price = re.findall(pattern, htmltext)
-    print ("current price of " + symbol + " is: " + str(price))
+    price = str(price)
+    price = price[2:-2]
+    query = "update stock set price='" + price + "' where symbol like '" + symbol + "';"
+    priCursor.execute(query)
+    db.commit()
+    print ("current price of " + symbol + " is: " + price)
     i += 1
 
-symbolfile.close()
+symCursor.close()
+priCursor.close()
